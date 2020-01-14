497c4789195c9f84c3575e1db301b9a4012642ab
inna_yan <inna_yan@TeamCity>
2020-01-14 17:46:58 +0300

    TeamCity change in 'Report tab per branch' project: 'https://github.com/innayan/myrepository/blob/asasas' VCS root was created

--- .teamcity/patches/buildTypes/NotDefaultBranch.kts
+++ .teamcity/patches/buildTypes/NotDefaultBranch.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("NotDefaultBranch")
     name = "Not default branch"
+
+    vcs {
+        root(RelativeId("HttpsGithubComInnayanMyrepositoryBlobAsasas"))
+    }
 }))
 
